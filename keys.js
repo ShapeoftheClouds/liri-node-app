ca1f9d7ea3a8c642812b7072d168ffedb1791ec4
ShapeoftheClouds <amandagracebutt@gmail.com>
2017-07-23 12:20:24 -0600

    adding twitter function

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'zoWljnARFIfnMnI71ETqb2cts',
+  consumer_secret: 'SMgP3P8RqyxBSRdigb8PNMdw5uYV7XPNX9DqnUk2QSVZMSeWEz',
+  access_token_key: '886277973225205760-PoecM3tLWo9gwxv1pSExRyP6EhHQELX',
+  access_token_secret: 'duPU9Z6vZ6eqexat6A6mvq1O9jjzqM1JkpEgADwKrBdiM',
+}
---
+++ liri.js
@@ -0,0 +1,31 @@
+var Twitter = require('twitter');
+var keysJS = require("./keys.js");
+var keys = keysJS.twitterKeys;
+
+var action = process.argv[2];
+
+// A switch statement that will direct which function gets run.
+switch (action) {
+	case "my-tweets":
+	twitter();
+	break;
+
+	case "spotify-this-song":
+	break;
+
+	case "movie-this":
+	break;
+
+	case "do-what-it-says":
+	break;
+}
+
+// Function pulling tweets from twitter
+function twitter() {
+var client = new Twitter(keys);
+var params = {screen_name: 'ShapeoftheCloud', count: 20}; 
+client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
+		console.log(tweets); 
+		console.log(response);
+});
+}
